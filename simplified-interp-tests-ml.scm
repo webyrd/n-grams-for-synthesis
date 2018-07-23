1e7518336583bd55656bf5ec26bfcb789f233a57
William Byrd <webyrd@gmail.com>
2018-07-23 01:40:39 -0500

    working on MLish variant of interpreter, based on work with Kanae Tsushima

--- simplified-interp-tests-ml.scm
+++ simplified-interp-tests-ml.scm
@@ -369,6 +369,62 @@
         (if (null? l)
             s
             (cons (car l) (@ append (cdr l) s)))))))
+
+  (test "append-15b"
+    (run 1 (prog)
+      (fresh (q r s t u)
+        (absento 1 prog)
+        (absento 2 prog)
+        (absento 3 prog)
+        (absento 4 prog)
+        (absento 5 prog)
+        (absento 6 prog)
+        (== `(lambda (l s)
+               (if ,q
+                   ,r
+                   (,s (car l) (@ append ,t ,u))))
+            prog)
+        (evalo
+         `(letrec ((append ,prog))
+            (list
+             (@ append nil nil)
+             (@ append (cons 1 nil) (cons 2 nil))
+             (@ append (cons 3 (cons 4 nil)) (cons 5 (cons 6 nil)))))
+         '(nil
+           (cons 1 (cons 2 nil))
+           (cons 3 (cons 4 (cons 5 (cons 6 nil))))))))
+   '(((lambda (l s)
+        (if (null? l)
+            s
+            (cons (car l) (@ append (cdr l) s)))))))
+
+  (test "append-16"
+    (run 1 (prog)
+      (fresh (q r s t)
+        (absento 1 prog)
+        (absento 2 prog)
+        (absento 3 prog)
+        (absento 4 prog)
+        (absento 5 prog)
+        (absento 6 prog)
+        (== `(lambda (l s)
+               (if ,q
+                   ,r
+                   (,s (car l) ,t)))
+            prog)
+        (evalo
+         `(letrec ((append ,prog))
+            (list
+             (@ append nil nil)
+             (@ append (cons 1 nil) (cons 2 nil))
+             (@ append (cons 3 (cons 4 nil)) (cons 5 (cons 6 nil)))))
+         '(nil
+           (cons 1 (cons 2 nil))
+           (cons 3 (cons 4 (cons 5 (cons 6 nil))))))))
+   '(((lambda (l s)
+        (if (null? l)
+            s
+            (cons (car l) (@ append (cdr l) s)))))))
   
  #|
  (test "append-16"
