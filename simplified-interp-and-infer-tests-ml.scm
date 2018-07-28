d11d67fab12a98726402d8678cb5cab96f444491
William Byrd <webyrd@gmail.com>
2018-07-28 00:34:17 -0500

    testing ML langauge

--- simplified-interp-and-infer-tests-ml.scm
+++ simplified-interp-and-infer-tests-ml.scm
@@ -1,7 +1,7 @@
 (test-runner
  ;; timeout in seconds
  10
-
+ 
  (test "append-eval-only-14"
    (run 1 (prog)
      (fresh (q r s)
@@ -356,7 +356,88 @@
           (if (null? l)
               s
               (cons (car l) ((append (cdr l)) s))))))))
- 
+
+  (test "append-eval-only-18"
+    (run 1 (prog)
+      (fresh (q)
+        (absento 1 prog)
+        (absento 2 prog)
+        (absento 3 prog)
+        (absento 4 prog)
+        (absento 5 prog)
+        (absento 6 prog)
+        (== `(lambda (l s)
+               ,q)
+            prog)
+        (evalo
+         `(letrec ((append ,prog))
+            (list
+             (append nil nil)
+             (append (cons 1 nil) (cons 2 nil))
+             (append (cons 3 (cons 4 nil)) (cons 5 (cons 6 nil)))))
+         '(nil
+           (cons 1 (cons 2 nil))
+           (cons 3 (cons 4 (cons 5 (cons 6 nil))))))))
+    '(((lambda (l s)
+         (if (null? l)
+             s
+             (cons (car l) (append (cdr l) s)))))))
+
+  (test "append-eval-only-curried-18"
+    (run 1 (prog)
+      (fresh (q)
+        (absento 1 prog)
+        (absento 2 prog)
+        (absento 3 prog)
+        (absento 4 prog)
+        (absento 5 prog)
+        (absento 6 prog)
+        (== `(lambda (l)
+               (lambda (s)
+                 ,q))
+            prog)
+        (evalo
+         `(letrec ((append ,prog))
+            (list
+             ((append nil) nil)
+             ((append (cons 1 nil)) (cons 2 nil))
+             ((append (cons 3 (cons 4 nil))) (cons 5 (cons 6 nil)))))
+         '(nil
+           (cons 1 (cons 2 nil))
+           (cons 3 (cons 4 (cons 5 (cons 6 nil))))))))
+    '(((lambda (l)
+         (lambda (s)
+           (if (null? l)
+               s
+               (cons (car l) ((append (cdr l)) s))))))))
+
+  (test "append-eval-only-cons-no-list-18"
+    (run 1 (prog)
+      (fresh (q)
+        (absento 1 prog)
+        (absento 2 prog)
+        (absento 3 prog)
+        (absento 4 prog)
+        (absento 5 prog)
+        (absento 6 prog)
+        (== `(lambda (l s)
+               ,q)
+            prog)
+        (evalo
+         `(letrec ((append ,prog))
+            (cons (append nil nil)
+                  (cons (append (cons 1 nil) (cons 2 nil))
+                        (cons (append (cons 3 (cons 4 nil)) (cons 5 (cons 6 nil)))
+                              nil))))
+         '(cons nil
+                (cons (cons 1 (cons 2 nil))
+                      (cons (cons 3 (cons 4 (cons 5 (cons 6 nil))))
+                            nil))))))
+    '(((lambda (l s)
+         (if (null? l)
+             s
+             (cons (car l) (append (cdr l) s)))))))
+  
  (test "append-eval-only-curried-cons-no-list-18"
    (run 1 (prog)
      (fresh (q)
