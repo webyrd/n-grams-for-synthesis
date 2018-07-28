66b01a548d51137fb23b617e4d1e5d7ebe49e4b9
William Byrd <webyrd@gmail.com>
2018-07-28 01:42:45 -0500

    testing ML langauge

--- simplified-interp-and-infer-tests-ml.scm
+++ simplified-interp-and-infer-tests-ml.scm
@@ -56,7 +56,7 @@
                     (list (append nil nil)
                           (append (cons 1 nil) (cons 2 nil))
                           (append (cons 3 (cons 4 nil)) (cons 5 (cons 6 nil))))))
-           expr)
+           expr)       
        (evalo
         expr
         `(pair (closure . ,clos)
@@ -67,6 +67,39 @@
         (if (null? l)
             s
             (cons (car l) (append (cdr l) s))))))) 
+
+ (test "append-infer-and-eval-only-14"
+   (run 1 (prog)
+     (fresh (q r s expr)
+       (absento 1 prog)
+       (absento 2 prog)
+       (absento 3 prog)
+       (absento 4 prog)
+       (absento 5 prog)
+       (absento 6 prog)       
+       (== `(lambda (l s)
+              (if ,q
+                  ,r
+                  (,s (car l) (append (cdr l) s))))
+           prog)
+       (== `(letrec ((append ,prog))
+              (list
+               (append nil nil)
+               (append (cons 1 nil) (cons 2 nil))
+               (append (cons 3 (cons 4 nil)) (cons 5 (cons 6 nil)))))
+           expr)
+       (type-expo
+        expr
+        `(list (list int)))
+       (evalo
+        expr
+        '(nil
+          (cons 1 (cons 2 nil))
+          (cons 3 (cons 4 (cons 5 (cons 6 nil))))))))
+   '(((lambda (l s)
+        (if (null? l)
+            s
+            (cons (car l) (append (cdr l) s)))))))
  
  (test "append-eval-only-14"
    (run 1 (prog)
