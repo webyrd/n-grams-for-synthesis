737a17c539ac2a00d517a79cf43905a5139c7e64
William Byrd <webyrd@gmail.com>
2018-07-30 17:42:01 -0500

    updated corpus to use code from SRFIs

--- corpus.scm
+++ corpus.scm
@@ -27,16 +27,381 @@
 ;; 'bigrams-for-expr' function will be counted as "generic" procedure
 ;; applications
 
-
 (define exprs
   '(
 
-    (define reverse
-      (lambda (ls)
-        (if (null? ls)
-            '()
-            (append (reverse (cdr ls)) (list (car ls))))))
+    ;;; Adapted from subset of functions from SRFIs
+
+    (define proper-list?
+      (lambda (x lag)
+        (if (pair? x)
+            (let ((x (cdr x)))
+              (if (pair? x)
+                  (let ((x   (cdr x))
+                        (lag (cdr lag)))
+                    (and (not (equal? x lag)) (proper-list? x lag)))
+                  (null? x)))
+            (null? x))))
+
+    (define dotted-list?-aux
+      (lambda (x lag)
+        (if (pair? x)
+            (let ((x (cdr x)))
+              (if (pair? x)
+                  (let ((x   (cdr x))
+                        (lag (cdr lag)))
+                    (and (not (equal? x lag)) (dotted-list?-aux x lag)))
+                  (not (null? x))))
+            (not (null? x)))))
+
+    (define circular-list?-aux
+      (lambda (x lag)
+        (and (pair? x)
+             (let ((x (cdr x)))
+               (and (pair? x)
+                    (let ((x   (cdr x))
+                          (lag (cdr lag)))
+                      (or (equal? x lag) (circular-list?-aux x lag))))))))
+
+    (define not-pair?
+      (lambda (x)
+        (not (pair? x))))
+
+    (define null-list?
+      (lambda (l)
+        (if (pair? l)
+            #f
+            (if (null? l)
+                #t
+                (void)))))
+
+    (define list=-aux
+      (lambda (list-a others)
+        (or (null? others)
+            (let ((list-b (car others))
+                  (others (cdr others)))
+              (if (equal? list-a list-b) ; EQUAL? => LIST=
+                  (list=-aux list-b others)
+                  )))))
+
+    (define list=-aux-aux
+      (lambda (pair-a pair-b)
+        (if (null-list? pair-a)
+            (and (null-list? pair-b)
+                 (list=-aux list-b others))
+            (and (not (null-list? pair-b))
+                 (= (car pair-a) (car pair-b))
+                 (list=-aux-aux (cdr pair-a) (cdr pair-b))))))
+
+    (define length+-aux
+      (lambda (x lag len)               ; Returns #f if X is circular.
+        (if (pair? x)
+            (let ((x (cdr x))
+                  (len (+ len 1)))
+              (if (pair? x)
+                  (let ((x   (cdr x))
+                        (lag (cdr lag))
+                        (len (+ len 1)))
+                    (and (not (equal? x lag)) (length+-aux x lag len)))
+                  len))
+            len)))
+
+    (define last-pair-aux
+      (lambda (lis)
+        (let ((tail (cdr lis)))
+          (if (pair? tail) (last-pair-aux tail) lis))))
+
+    (define unzip1
+      (lambda (lis)
+        (map car lis)))
+
+    (define reduce
+      (lambda (f ridentity lis)
+        (if (null-list? lis) ridentity
+            (fold f (car lis) (cdr lis)))))
+
+    (define alist-cons
+      (lambda (key datum alist)
+        (cons (cons key datum) alist)))
+
+    (define alist-copy
+      (lambda (alist)
+        (map (lambda (elt) (cons (car elt) (cdr elt)))
+             alist)))
+
+    (define find
+      (lambda (pred list)
+        (let ((v (find-tail pred list)))
+          (if v
+              (car v)
+              #f))))
+
+    (define break
+      (lambda (pred lis)
+        (span (lambda (x) (not (pred x))) lis)))
+
+    (define lset-intersection
+      (lambda (= lis1 lists)
+        (let ((lists (delete lis1 lists equal?))) ; Throw out any LIS1 vals.
+          (if (any null-list? lists)
+              '()
+              (if (null? lists)
+                  lis1
+                  (filter (lambda (x)
+                            (every (lambda (lis) (member x lis =)) lists))
+                          lis1))))))
+
+    (define lset-difference
+      (lambda (= lis1 lists)
+        (let ((lists (filter pair? lists))) ; Throw out empty lists.
+          (if (null? lists)
+              lis1
+              (if (memq lis1 lists)
+                  '()
+                  (filter (lambda (x)
+                            (every (lambda (lis) (not (member x lis =)))
+                                   lists))
+                          lis1))))))
+
+    (define lset-xor
+      (lambda (= lists)
+        (reduce (lambda (b a)           ; Compute A xor B:
+                  ;; Note that this code relies on the constant-time
+                  ;; short-cuts provided by LSET-DIFF+INTERSECTION,
+                  ;; LSET-DIFFERENCE & APPEND to provide constant-time short
+                  ;; cuts for the cases A = (), B = (), and A equal? B. It takes
+                  ;; a careful case analysis to see it, but it's carefully
+                  ;; built in.
+
+                  ;; Compute a-b and a^b, then compute b-(a^b) and
+                  ;; cons it onto the front of a-b.
+                  (receive (a-b a-int-b)   (lset-diff+intersection = a b)
+                    (if (null? a-b)
+                        (lset-difference = b a)
+                        (if (null? a-int-b)
+                            (append b a)
+                            (fold (lambda (xb ans)
+                                    (if (member xb a-int-b =) ans (cons xb ans)))
+                                  a-b
+                                  b)))))
+                '() lists)))
+
+    (define look-up
+      (lambda (key alist)
+        (and-let* ((x (assq key alist))) (cdr x))))
+
+    (define lset-adjoin
+      (lambda (= lis elts)
+        (foldl (lambda (elt ans) (if (mem = elt ans) ans (cons elt ans)))
+               lis elts)))
+
+    (define lset-union
+      (lambda (= lists)
+        (if (pair? lists)
+            (foldl (lambda (lis ans)	; Compute LIS + ANS.
+                     (if (pair? ans)
+                         (foldl (lambda (elt ans) (if (mem = elt ans) ans
+                                                      (cons elt ans)))
+                                ans lis)
+                         lis))	; Don't copy LIS if you don't have to.
+                   (car lists)
+                   (cdr lists))
+            '())))
+
+    (define lset-intersection
+      (lambda (= lis1 lists)
+        (if (every pair? lists)
+            (foldl (lambda (lis residue) (filter (lambda (x) (mem = x lis)) residue))
+                   lis1
+                   lists)
+            '())))       ; Don't do unnecessary scans of LIS1/RESIDUE.
+
+    (define lset-difference
+      (lambda (= lis1 lists)
+        (foldl (lambda (lis residue)
+                 (if (pair? lis)
+                     (filter (lambda (x) (not (mem = x lis))) residue)
+                     residue)) ; Don't do unnecessary scans of RESIDUE.
+               lis1
+               lists)))
+
+    (define lset-xor
+      (lambda (= lists)
+        (if (pair? lists) ; We don't really have to do this test, but WTF.
+            (foldl (lambda (a b)        ; Compute A xor B:
+                     (if (pair? a)
+                         ;; Compute a-b and a^b, then compute b-(a^b) and
+                         ;; cons it onto the front of a-b.
+                         (receive (a-b a-int-b) (lset-diff+intersection = a b)
+                           (foldl (lambda (xb ans)
+                                    (if (mem = xb a-int-b) ans (cons xb ans)))
+                                  a-b
+                                  b))
+                         b))       ; Don't copy B if we don't have to.
+                   (car lists)
+                   (cdr lists)) 
+            '())))
+
+    (define my-vector?
+      (lambda (x)
+        (and (ur-vector? x)
+             (not (input-string? x))
+             (not (output-string? x)))))
+
+    (define input-string?
+      (lambda (x)
+        (and (ur-vector? x)
+             (positive? (ur-vector-length x))
+             (equal? input-string-tag (ur-vector-ref x 0)))))
+
+    (define output-string?
+      (lambda (x)
+        (and (ur-vector? x)
+             (positive? (ur-vector-length x))
+             (equal? output-string-tag (ur-vector-ref x 0)))))
+
+    (define process-cond-clauses
+      (lambda (clauses)
+        (if (null? clauses)
+            (exit #f)
+            (if (or (and (equal? (caar clauses) 'else)
+                         (null? (cdr clauses)))
+                    (satisfied? (caar clauses)))
+                (process-clauses (cdar clauses))
+                (process-cond-clauses (cdr clauses))))))
+
+    (define satisfied?
+      (lambda (requirement)
+        (if (pair? requirement)
+            (let ((v (car requirement)))
+              (if (equal? 'and v)
+                  (all-satisfied? (cdr requirement))
+                  (if (equal? 'or v)
+                      (any-satisfied? (cdr requirement))
+                      (if (equal? 'not v)
+                          (not (satisfied? (cadr requirement)))
+                          (void)))))
+            (memq requirement features))))
+
+    (define all-satisfied?
+      (lambda (list)
+        (if (null? list)
+            #t
+            (and (satisfied? (car list))
+                 (all-satisfied? (cdr list))))))
+
+    (define any-satisfied?
+      (lambda (list)
+        (if (null? list)
+            #f
+            (or (satisfied? (car list))
+                (any-satisfied? (cdr list))))))
+
+    (define eval-and-clause?
+      (lambda (req-list)
+        (or (null? req-list)
+            (and (eval-feature-requal? (car req-list))
+                 (eval-and-clause? (cdr req-list))))))
+
+    (define eval-or-clause?
+      (lambda (req-list)
+        (and (not (null? req-list))
+             (or (eval-feature-requal? (car req-list))
+                 (eval-or-clause? (cdr req-list))))))
+
+    (define eval-not-clause?
+      (lambda (req)
+        (not (eval-feature-requal? req))))
+
+    (define eval-feature-requal?
+      (lambda (feature-req)
+        (if (not (pair? feature-req))
+            (feature-present? feature-req)
+            (if (equal? 'and (car feature-req))
+                (eval-and-clause? (cdr feature-req))
+                (if (equal? 'or (car feature-req))
+                    (eval-or-clause? (cdr feature-req))
+                    (if (equal? 'not (car feature-req))
+                        (apply eval-not-clause? (cdr feature-req))
+                        (void)))))))
+
+    (define time-difference
+      (lambda (time1 time2)
+        (tm:time-difference time1 time2 (make-time #f #f #f))))
+
+    (define add-duration
+      (lambda (time1 duration)
+        (tm:add-duration time1 duration (make-time (time-type time1) #f #f))))
+
+    (define subtract-duration
+      (lambda (time1 duration)
+        (tm:subtract-duration time1 duration (make-time (time-type time1) #f #f))))
+
+    (define tm:encode-julian-day-number
+      (lambda (day month year)
+        (let* ((a (quotient (- 14 month) 12))
+               (y (- (- (+ year 4800) a) (if (negative? year) -1 0)))
+               (m (- (+ month (* 12 a)) 3)))
+          (+ day
+             (quotient (+ (* 153 m) 2) 5)
+             (* 365 y)
+             (quotient y 4)
+             (- (quotient y 100))
+             (quotient y 400)
+             -32045))))
+
+    (define tm:char-pos
+      (lambda (char str index len)
+        (if (>= index len)
+            #f
+            (if (char=? (string-ref str index) char)
+                index
+                (tm:char-pos char str (+ index 1) len)))))
     
+    (define array-length
+      (lambda (arr dim)
+        (- (array-end arr dim)
+           (array-start arr dim))))
+
+    (define shape-for-each
+      (lambda (shp proc o)
+        (if (null? o)
+            (array:arlib:shape-for-each/arguments shp proc)
+            (if (vector? (car o))
+                (array:arlib:shape-for-each/vector shp proc (car o))
+                (array:arlib:shape-for-each/array shp proc (car o))))))
+
+    (define array-for-each-index
+      (lambda (arr proc o)
+        (if (null? o)
+            (array:arlib:array-for-each-index/arguments arr proc)
+            (if (vector? (car o))
+                (array:arlib:array-for-each-index/vector arr proc (car o))
+                (array:arlib:array-for-each-index/array arr proc (car o))))))
+
+    (define sub-dup
+      (lambda ()
+        (let ((c (read-char)))
+          (if (not (eof-object? c))
+              ((char->dupper c) c)))))
+
+    (define char->dupper
+      (lambda (char)
+        (let ((v (assq char *read-alist*)))
+          (if v
+              (cdr v)
+              write-char))))
+
+    (define is-char-terminating?
+      (lambda (char)
+        (let ((v (assq char *read-terminating?-alist*)))
+          (if v
+              (cdr v)
+              #t))))
+
+
+    ;;;  Simple recursive Scheme functions (Little Schemer Style)
+
     (define foldr
       (lambda (f base ls)
         (if (null? ls)
@@ -67,12 +432,6 @@
             (if (equal? (car (car ls)) x)
                 (cdr (car ls))
                 (lookup x (cdr ls))))))
-
-    (define append
-      (lambda (ls1 ls2)
-        (if (null? ls1)
-            ls2
-            (cons (car ls1) (append (cdr ls1) ls2)))))
 
     (define map
       (lambda (p ls)
@@ -120,6 +479,7 @@
                 (cons (car ls) (remove (cdr ls)))))))
 
     ;; little schemer definitions
+
     (define o+
       (lambda (n m)
         (if (zero? m)
@@ -470,5 +830,6 @@
                     ))))
         (eval-expr '((lambda (x) `(,x ',x)) '(lambda (x) `(,x ',x)))
                    'initial-env)))
-    
+
+
     ))
