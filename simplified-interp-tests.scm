3e21dab8a028484e976ec56507f0b47bd849d507
Rob Zinkov <rob@zinkov.com>
2018-09-04 17:06:32 +0100

    Add rember

--- simplified-interp-tests.scm
+++ simplified-interp-tests.scm
@@ -410,6 +410,23 @@
       (((lambda (_.0 _.1) (if (null? _.0) _.1 (cons (car _.0) (append (cdr _.0) _.1)))) (=/= ((_.0 _.1)) ((_.0 a)) ((_.0 and)) ((_.0 append)) ((_.0 b)) ((_.0 c)) ((_.0 car)) ((_.0 cdr)) ((_.0 cons)) ((_.0 d)) ((_.0 e)) ((_.0 equal?)) ((_.0 f)) ((_.0 if)) ((_.0 lambda)) ((_.0 letrec)) ((_.0 list)) ((_.0 match)) ((_.0 not)) ((_.0 null?)) ((_.0 or)) ((_.0 quote)) ((_.0 symbol?)) ((_.1 a)) ((_.1 and)) ((_.1 append)) ((_.1 b)) ((_.1 c)) ((_.1 car)) ((_.1 cdr)) ((_.1 cons)) ((_.1 d)) ((_.1 e)) ((_.1 equal?)) ((_.1 f)) ((_.1 if)) ((_.1 lambda)) ((_.1 letrec)) ((_.1 list)) ((_.1 match)) ((_.1 not)) ((_.1 null?)) ((_.1 or)) ((_.1 quote)) ((_.1 symbol?))) (sym _.0 _.1)))))   
    )
 
+;; rember testx
+
+(test "rember-1"
+      (run 1 (q)
+	   (evalo
+	    `(letrec ((rember
+		       (lambda (a l)
+			 (cond
+			  ((null? l) (quote ()))
+			  ((eq? (car l) a) (cdr (l))
+			   (else (cons (car l)
+				       (rember a (cdr l)))))))))
+	       (rember 'a '(a b c)))
+	    q))
+      '((b c)))
+	    
+ 
 ;; reverse tests
 
 (test "reverse-1"
