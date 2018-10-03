0da22b8f37e771e69c8856fe1676aa3ac18dc537
William Byrd <webyrd@gmail.com>
2018-10-02 20:18:12 -0500

    working on combined inference and evaluation

---
+++ simplified-interp-tests-ml-infer-evalo.scm
@@ -0,0 +1,9 @@
+(test-runner
+ ;; timeout in seconds
+ 10
+
+ ;; add tests here!
+ 
+ )
+
+(exit)
---
+++ variant-dynamic-ordering-ml-infer-evalo.scm
@@ -0,0 +1,13 @@
+(define *output-table-file-name* "tmp/variant-dynamic-ordering-ml-!-evalo-table.scm")
+
+(define allow-incomplete-search? #f)
+
+(define lookup-optimization? #f)
+
+(load "mk-vicare.scm")
+(load "mk.scm")
+(load "test-check.scm")
+(load "interp-core-ml-!-evalo.scm")
+(load "construct-ordering-ml-!-evalo.scm")
+(load "interp-simplified-dynamic-ml-!-evalo.scm")
+(load "simplified-interp-tests-ml-!-evalo.scm")
