35007d883ec0c3c0e592a7e17c2c3eedb4eb845b
William Byrd <webyrd@gmail.com>
2018-10-02 23:15:45 -0500

    added optimized combined inferencer and evaluator

---
+++ combined-dynamic-ordering-with-application-and-lookup-optimizations-ml-infer-evalo.scm
@@ -0,0 +1,13 @@
+(define *output-table-file-name* "tmp/combined-dynamic-ordering-with-application-and-lookup-optimizations-ml-infer-evalo-table.scm")
+
+(define allow-incomplete-search? #f)
+
+(define lookup-optimization? #f)
+
+(load "mk-vicare.scm")
+(load "mk.scm")
+(load "test-check.scm")
+(load "combined-app-optimiztion-ml-infer-evalo.scm")
+(load "combined-construct-ordering-ml-infer-evalo.scm")
+(load "combined-simplified-dynamic-ml-infer-evalo.scm")
+(load "combined-simplified-tests-ml-infer-evalo.scm")
--- test-variants.scm
+++ test-variants.scm
@@ -29,7 +29,7 @@
                           ;;; inferencers should be run separately.
 
                           #| Combined inferencer/evaluator for ML-like language |#
-                          ;"combined-dynamic-ordering-with-application-and-lookup-optimizations-ml-infer-evalo"
+                          "combined-dynamic-ordering-with-application-and-lookup-optimizations-ml-infer-evalo"
                           "combined-dynamic-ordering-ml-infer-evalo"
                           ;"combined-expert-ordering-ml-infer-evalo"
                           
@@ -58,13 +58,12 @@
                           
                           #| Interpreters for ML-like language: |#
                           
-                          ; "variant-dynamic-ordering-with-application-and-lookup-optimizations-ml"
+                          ;"variant-dynamic-ordering-with-application-and-lookup-optimizations-ml"
                           ;"variant-dynamic-ordering-ml"
                           ;"variant-expert-ordering-with-application-and-lookup-optimizations-ml"
                           
                           #|
-                          "variant-dynamic-ordering-with-application-optimization-ml"
-                          
+                          "variant-dynamic-ordering-with-application-optimization-ml"                          
                           "variant-expert-ordering-with-application-optimization-ml"
                           "variant-expert-ordering-ml"                          
                           |#
